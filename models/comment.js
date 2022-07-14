a9403aa5f8b4fd0ba3a2577fe48742af01ead775
Amir Khan <ak2022dev@gmail.com>
2022-07-14 14:59:07 +0100

    Test drive for Comment class first test passed refactored to use DB

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,18 @@
 // File: comment.js
 
-class Comment {
-  constructor(text) {
-    this.text = text;
-  }
-}
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  text: String,
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+
+//class Comment {
+// constructor(text) {
+//    this.text = text;
+//  }
+//}
 
 module.exports = Comment;
--- spec/models/comment.spec.js
+++ spec/models/comment.spec.js
@@ -7,20 +7,20 @@ require("../mongodb_helper");
 var Comment = require("../../models/comment");
 
 describe("Comment model", () => {
-//   beforeEach((done) => {
-//     mongoose.connection.collections.comments.drop(() => {
-//       done();
-//     });
-//   });
-
-//  it("has a comment", () => {
-//    var comment = new Comment({ text: "some comment"});
-//    expect(comment.text).toEqual("some comment");
-//  });
+   beforeEach((done) => {
+     mongoose.connection.collections.comments.drop(() => {
+       done();
+     });
+   });
 
   it("has a comment", () => {
-    var comment = new Comment("some comment");
+    var comment = new Comment({ text: "some comment"});
     expect(comment.text).toEqual("some comment");
   });
+
+//  it("has a comment", () => {
+//    var comment = new Comment("some comment");
+//    expect(comment.text).toEqual("some comment");
+//  });
 
 });
