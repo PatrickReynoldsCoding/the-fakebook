6eb4050f56462a31f50c578c9067e64cfc2c73b6
Amir Khan <ak2022dev@gmail.com>
2022-07-14 14:47:44 +0100

    Test drive for Comment class first test passed (constructor)

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+// File: comment.js
+
+class Comment {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+module.exports = Comment;
---
+++ spec/models/comment.spec.js
@@ -0,0 +1,26 @@
+// File: spec/models/comment.spec.js
+
+var mongoose = require("mongoose");
+
+require("../mongodb_helper");
+
+var Comment = require("../../models/comment");
+
+describe("Comment model", () => {
+//   beforeEach((done) => {
+//     mongoose.connection.collections.comments.drop(() => {
+//       done();
+//     });
+//   });
+
+//  it("has a comment", () => {
+//    var comment = new Comment({ text: "some comment"});
+//    expect(comment.text).toEqual("some comment");
+//  });
+
+  it("has a comment", () => {
+    var comment = new Comment("some comment");
+    expect(comment.text).toEqual("some comment");
+  });
+
+});
